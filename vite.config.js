188a20aa7dce0aefa5614d98e96f5411662d7ee3
Marcusk01 <mushroomkase@gmail.com>
2025-06-14 02:16:44 -0400

    updated deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
   server: {
     host: '0.0.0.0',
     port: 54663,
