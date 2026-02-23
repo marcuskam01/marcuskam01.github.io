96a70d242375d399e00d01b53636af75e5e37100
Marcusk01 <mushroomkase@gmail.com>
2026-02-23 00:16:44 -0500

    updated reading list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     >
       <h1>What I'm Building</h1>
       <p>Welcome to my personal website! Here I showcase some of my projects and developments. Let me know if you find any of them interesting!</p>
-      
+
       {/* Add your projects here */}
       <motion.div
         initial={{ opacity: 0 }}
@@ -19,19 +19,19 @@ const Home = () => {
         transition={{ delay: 0.2 }}
       >
         <h2>Current Projects</h2>
-        <ProjectItem 
+        <ProjectItem
           title="DexSuite - Dexterous hand control"
           description="Building with Python, Gymnasium, and Genesis. Coming soon"
           link="https://github.com/marcuskam01/"
         />
 
         <h2>Past Projects</h2>
-        <ProjectItem 
+        <ProjectItem
           title="StayAwake - Real time drowsiness detector"
           description="A real-time computer vision application that monitors driver alertness using facial landmarks and eye closure detection, alerting drivers to prevent drowsiness-related accidents. Winner of 2022 CodeJam and a qualifier for 2023 Schulich Hacks."
           link="https://github.com/marcuskam01/"
         />
-        <ProjectItem 
+        <ProjectItem
           title="Capstone - Treatment analytics for McGill University Health Center"
           description="Developed an analytics platform for clinicians to analyze patient-reported treatment outcomes and optimize care plans for sickle cell anemia patients with chronic pain unrelieved by standard medication. Provided data-driven insights to support personalized treatment planning."
           link="https://github.com/marcuskam01/"
@@ -44,6 +44,7 @@ const Home = () => {
       >
         <h2>Reading List</h2>
         <ul>
+          <li>Practical MLOps: Operationalizing Machine Learning Models</li>
           <li>The Data Warehouse Toolkit: The Definitive Guide to Dimensional Modeling</li>
           <li>Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems</li>
         </ul>
